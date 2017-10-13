b2056369f5e0b64f68285258371b07699d1aea86
Stefan Hellwig <sh@connext.de>
2017-10-13 08:01:57 +0200

    Ausgabestring verändert; using namespace entfernt; Kommentar erweitert.

--- HelloWorld/HelloWorld/main.cpp
+++ HelloWorld/HelloWorld/main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 
-using namespace std;
-
 auto main() -> int
 {
-   cout << "Hello World!\n";
+   std::cout << "Hello, World!" << std::endl;
 
-   // Auf einen Tastendruck warten
-   getchar();
+   // Auf einen Tastendruck warten, damit das Terminal mit der Ausgabe nicht sofort wieder geschlossen wird
+   std::getchar();
 
    return 0;
 }
